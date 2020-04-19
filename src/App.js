9f28db01a8d16f7307448914ddae698a788562b3
tavolafourcade <tavolafourcade@gmail.com>
2020-04-18 20:44:40 -0500

    Developed the main components: Navigation, Logo, Rank and ImageLinkForm

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
+import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
 import './App.css';
+// import { logDOM } from '@testing-library/react';
+import 'tachyons';
+import Particles from 'react-particles-js';
+
+const particleOptions = {
+  particles: {
+    number:{
+      value:100,
+      density:{
+        enable:true,
+        value_area: 500
+      }
+    }
+  },
+  interactivity:{
+    detect_on: "window",
+    events:{
+      onhover:{
+        enable: true,
+        mode: 'repulse'
+      }
+    }
+    
+  }
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Particles className="particles"
+        params={particleOptions}
+      />
+      <Navigation />
+      <Logo/>
+      <Rank/>
+      <ImageLinkForm/>
+      {/* {<FaceRecognition/>} */}
     </div>
   );
 }
---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+// const name = 'Octavio';
+const Rank = () => {
+    return(
+        <div>
+            <div className='white f3'>
+                {'Octavio, the total number of faces is...'}
+            </div>
+
+            <div className='white f1'>
+                {'#5'}
+            </div>
+            
+        </div>
+    );
+}
+
+export default Rank;
